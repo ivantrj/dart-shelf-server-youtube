fa38472281d2cc7adbf045f5b60b06b117f33570
Ivan Trj <ivantrajanovski@gmail.com>
2023-07-30 10:08:58 +0200

    first commit

---
+++ bin/dart_shelf_server_youtube.dart
@@ -0,0 +1,5 @@
+import 'package:dart_shelf_server_youtube/dart_shelf_server_youtube.dart' as dart_shelf_server_youtube;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_shelf_server_youtube.calculate()}!');
+}
---
+++ test/dart_shelf_server_youtube_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('cats', () {
+    expect(calculate(), 42);
+  });
+}
